5e83adc7b3108ed58c60a70b479dd8180963a57a
WindowsThinkPad <WindowsThinkPad@DESKTOP-2T2FUK2>
2022-01-06 13:11:03 +0100

    Add project files.

---
+++ WineShop/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WineShop.Models;
+
+namespace WineShop.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category>? Categories { get; set; }
+    }
+}
