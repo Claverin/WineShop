54cc91b7cfc6acb54ebf41453adb53d790a72341
Piotr Michalski <mrpiotrmichalski@gmail.com>
2022-01-10 12:13:31 +0100

    CRUD Implemented

---
+++ WineShop/Controllers/ProductTypeController.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WineShop.Data;
+using WineShop.Models;
+
+namespace WineShop.Controllers
+{
+    public class ProductTypeController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ProductTypeController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<ProductType> objList = _db.ProductType;
+            return View(objList);
+        }
+
+        //CREATE
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(ProductType obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.ProductType.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(obj);
+
+        }
+
+        //EDIT
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.ProductType.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(ProductType obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.ProductType.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(obj);
+
+        }
+
+        //DELETE
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.ProductType.Find(id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.ProductType.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.ProductType.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
