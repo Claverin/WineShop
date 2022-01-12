ebed4903b3b4642dc6b550f16ba9a8aff10ae64b
Piotr Michalski <mrpiotrmichalski@gmail.com>
2022-01-12 15:43:45 +0100

    PaymentMethod CRUD implemented

---
+++ WineShop/Models/PaymentMethod.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WineShop.Models
+{
+    public class PaymentMethod
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
