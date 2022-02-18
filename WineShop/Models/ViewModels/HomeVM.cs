ed1a6efeb13a42e2716309fca66558faea5fe5d5
Piotr Michalski <mrpiotrmichalski@gmail.com>
2022-02-18 23:10:59 +0100

    Implemented shop side

---
+++ WineShop/Models/ViewModels/HomeVM.cs
@@ -0,0 +1,8 @@
+﻿namespace WineShop.Models.ViewModels
+{
+    public class HomeVM
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public IEnumerable<ProductType> ProductTypes { get; set; }
+    }
+}
