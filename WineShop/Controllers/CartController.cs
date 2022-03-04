e944b5d8850bc9a6d5020fddeb43a80707e993b5
Piotr Michalski <mrpiotrmichalski@gmail.com>
2022-03-04 13:11:40 +0100

    Index CartController implemented

---
+++ WineShop/Controllers/CartController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WineShop.Data;
+using WineShop.Models;
+using WineShop.Utility;
+
+namespace WineShop.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CartController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+
+            List<ShoppingCart> shoppingCartList = new List<ShoppingCart>();
+            if(HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart) != null
+                && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart).Count()>0)
+            {
+                //session exists
+                shoppingCartList = HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart);
+            }
+
+            List<int> productInCart = shoppingCartList.Select(i => i.ProductId).ToList();
+            IEnumerable<Product> productList = _db.Product.Where(u => productInCart.Contains(u.Id));
+
+            return View(productList);
+        }
+    }
+}
