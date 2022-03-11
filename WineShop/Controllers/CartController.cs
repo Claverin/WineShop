657e82b13e34f9919cfddc1b76a75724de082144
Piotr Michalski <mrpiotrmichalski@gmail.com>
2022-03-11 12:36:53 +0100

    Cart Summary Implemented

--- WineShop/Controllers/CartController.cs
+++ WineShop/Controllers/CartController.cs
@@ -1,14 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 using WineShop.Data;
 using WineShop.Models;
+using WineShop.Models.ViewModels;
 using WineShop.Utility;
 
 namespace WineShop.Controllers
 {
+    [Authorize]
     public class CartController : Controller
     {
         private readonly ApplicationDbContext _db;
-
+        [BindProperty]
+        public ProductUserVM ProductUserVM { get; set; }
         public CartController(ApplicationDbContext db)
         {
             _db = db;
@@ -28,6 +33,39 @@ namespace WineShop.Controllers
             IEnumerable<Product> productList = _db.Product.Where(u => productInCart.Contains(u.Id));
 
             return View(productList);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Index")]
+        public IActionResult IndexPost()
+        {
+            return RedirectToAction(nameof(Summary));
+        }
+
+        public IActionResult Summary()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            List<ShoppingCart> shoppingCartList = new List<ShoppingCart>();
+            if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart) != null
+                && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart).Count() > 0)
+            {
+                //session exists
+                shoppingCartList = HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart);
+            }
+
+            List<int> productInCart = shoppingCartList.Select(i => i.ProductId).ToList();
+            IEnumerable<Product> productList = _db.Product.Where(u => productInCart.Contains(u.Id));
+
+            ProductUserVM = new ProductUserVM()
+            {
+                ApplicationUser = _db.ApplicationUser.FirstOrDefault(u => u.Id == claim.Value),
+                ProductList = productList.ToList()
+            };
+
+            return View(ProductUserVM);
         }
 
         public IActionResult Remove(int id)
---
+++ WineShop/Models/ViewModels/ProductUserVM.cs
@@ -0,0 +1,12 @@
+﻿namespace WineShop.Models.ViewModels
+{
+    public class ProductUserVM
+    {
+        public ProductUserVM()
+        {
+            ProductList = new List<Product>();
+        }
+        public ApplicationUser ApplicationUser { get; set; }
+        public IEnumerable<Product> ProductList { get; set; }
+    }
+}
